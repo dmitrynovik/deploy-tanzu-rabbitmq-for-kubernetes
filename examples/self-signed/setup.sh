a6d506f71fd5f688d77acdbda875aa52047359c8
dmitrtynovik <cryinstone@gmail.com>
2022-08-09 11:00:25 +1000

    fix: create namespace rabbitmq-system

--- examples/self-signed/setup.sh
+++ examples/self-signed/setup.sh
@@ -1,4 +1,6 @@
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.yaml 
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.yaml
+
+kubectl create namespace rabbitmq-system
 
 kubectl apply -n rabbitmq-system -f self-signed-issuer.yml
 kubectl apply -n rabbitmq-system -f self-signed-cluster-issuer.yml
