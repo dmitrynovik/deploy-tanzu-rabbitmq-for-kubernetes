09d57dbfd5a5632f179be8ca4dcc45bd7adc4768
dmitrtynovik <cryinstone@gmail.com>
2022-08-09 11:08:40 +1000

    fix: create ns rabbitmq-system only if N/A

--- examples/self-signed/setup.sh
+++ examples/self-signed/setup.sh
@@ -1,6 +1,6 @@
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.yaml
 
-kubectl create namespace rabbitmq-system
+kubectl create namespace rabbitmq-system --dry-run=client -o yaml | kubectl apply -f-
 
 kubectl apply -n rabbitmq-system -f self-signed-issuer.yml
 kubectl apply -n rabbitmq-system -f self-signed-cluster-issuer.yml
